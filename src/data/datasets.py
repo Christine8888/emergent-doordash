3f871bc63efbb70afc68e3cece42472db178f1c2
suzehva <suzeva@stanford.edu>
2025-11-11 14:06:09 -0800

    basic draft of querying api's for CoT

---
+++ src/data/datasets.py
@@ -0,0 +1,102 @@
+import hashlib
+import re
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from datasets import load_dataset
+from pydantic import BaseModel, Field
+
+NO_ANSWER_FOUND_ERROR = "<NO_ANSWER_FOUND>"
+
+
+class SavableBaseModel(BaseModel):
+    def save_to_file(self, file_path: str):
+        with open(file_path, "w") as f:
+            f.write(self.model_dump_json(indent=2))
+
+    @classmethod
+    def load_from_file(cls, file_path: str) -> "SavableBaseModel":
+        with open(file_path) as f:
+            return cls.model_validate_json(f.read())
+
+
+class ModelAnswer(SavableBaseModel):
+    cot: str
+    extracted_answer: str
+    is_correct: bool
+    prompt: Optional[str] = None
+
+class COT_Response(SavableBaseModel):
+    model: str
+    cot: str
+
+class Datum(SavableBaseModel):
+    id: str
+    ground_truth_answer: str
+    question: str
+    ground_truth_cot_responses: Optional[List[COT_Response]] = None # list of responses from different models
+    sampled_answers: Optional[List[ModelAnswer]] = None
+
+
+
+class Dataset(SavableBaseModel, ABC):
+    data: List[Datum]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> Datum:
+        return self.data[index]
+
+    @abstractmethod
+    def is_correct(self, model_answer: str, real_answer: str) -> bool: ...
+
+    @classmethod
+    @abstractmethod
+    def load_from_huggingface(cls) -> "Dataset": ...
+
+    @abstractmethod
+    def extract_answer(self, model_output: str) -> str:
+        # Extract the answer from the model output
+        ...
+
+
+class AIME2025(Dataset):
+    def is_correct(self, model_answer: str, real_answer: str) -> bool:
+        return model_answer.strip() == real_answer.strip()
+
+    @classmethod
+    def load_from_huggingface(cls) -> "AIME2025":
+        dataset_name = "opencompass/AIME2025"
+
+        configs = ["AIME2025-I", "AIME2025-II"]
+
+        ids: List[str] = []
+        questions: List[str] = []
+        answers: List[str] = []
+        for config in configs:
+            dataset = load_dataset(dataset_name, config, split="test")
+
+            for example in dataset:
+                answer = example["answer"]
+                numerical_answer = re.search(r"\d+", answer).group()  # type: ignore
+                answers.append(numerical_answer)
+
+                question = example["question"]
+                assert question not in questions, "Question already exists"
+                questions.append(question)
+
+                id = hashlib.md5(question.encode()).hexdigest()[:8]
+                assert id not in ids, "ID already exists"
+                ids.append(id)
+
+        return cls(
+            data=[Datum(id=id, question=question, ground_truth_answer=answer) for id, question, answer in zip(ids, questions, answers)],
+        )
+
+    def extract_answer(self, model_output: str) -> str:
+        # Select the last number in the model output
+        matches = re.findall(r"\d+", model_output)
+        if not matches:
+            return NO_ANSWER_FOUND_ERROR
+        return matches[-1]
--- suze_experiments/20251111/get_cot.py
+++ suze_experiments/20251111/get_cot.py
@@ -9,6 +9,11 @@ import hashlib
 import random
 import numpy as np
 import torch
+import random
+
+from src.data.datasets import AIME2025, Dataset
+from src.models.query_api import query_model_api
+
 
 def set_seed(seed: int):
     random.seed(seed)
@@ -16,51 +21,25 @@ def set_seed(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-class SavableBaseModel(BaseModel):
-    def save_to_file(self, file_path: str):
-        with open(file_path, "w") as f:
-            f.write(self.model_dump_json(indent=2))
-
-    @classmethod
-    def load_from_file(cls, file_path: str) -> "SavableBaseModel":
-        with open(file_path) as f:
-            return cls.model_validate_json(f.read())
-
-class COT_Response(SavableBaseModel):
-    model: str
-    cot: str
-
-class Datum(SavableBaseModel):
-    id: str
-    ground_truth_answer: str
-    question: str
-    ground_truth_cot_responses: Optional[List[COT_Response]] = None # list of responses from different models
-
 
 def main():
-    cot_dataset: list[Datum] = []
-    ids: List[str] = []
-    # get question and answer to query a model with
-    dataset_name = "opencompass/AIME2025"
-    for config in ['AIME2025-I', 'AIME2025-II']:
-        dataset = load_dataset(dataset_name, config, split="test")
-
-        for row in dataset:
-            question, ground_truth_answer = row['question'], row['answer']
-
-            id = hashlib.md5(question.encode()).hexdigest()[:8]
-            assert id not in ids, "ID already exists"
-            ids.append(id)
-
-            cot_dataset.append(Datum(id=id, question=question, ground_truth_answer=ground_truth_answer))
-            break
+    cot_dataset: Dataset = AIME2025.load_from_huggingface()
+    
 
     # choose one question to query model with
-    import random
     cot_sample = random.choice(cot_dataset)
     print(cot_sample)
 
     # query a model with one specific question
+    # models = ["o3-2025-04-16", "gpt-5-2025-08-07", "claude-opus-4-1", "claude-sonnet-4-5"]
+    models = ["gpt-5-nano-2025-08-07"]
+
+    for model in models:
+        result = query_model_api(cot_sample.question, model)
+        print(result)
+        # cot_sample.ground_truth_cot_responses.append(COT_Response(model=model, cot=result.response_text))
+
+
     
 
 
