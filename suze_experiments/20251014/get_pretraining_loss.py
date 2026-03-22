083a50efa7700c4a15dde72cb48fdafcdab33db8
suzehva <suzeva@stanford.edu>
2026-03-21 18:13:26 -0700

    fresh start

--- suze_experiments/20251014/get_pretraining_loss.py
+++
@@ -1,3 +0,0 @@
-from utils.setup import setup_env
-from huggingface_hub import list_models
-
--- suze_experiments/20251026/decomposed_data.py
+++
@@ -1,5 +0,0 @@
-"""
-x axis: either time model was released (for e.g. openai/anthropic models) or eg model size for models within the same family
-y axis: accuracy 
-draw multiple lines, all consisting of easier versions of some benchmark
-"""
